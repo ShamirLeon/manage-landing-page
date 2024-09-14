f53ab1195de764767cd6be402f12b1f8c31188b7
Shamir Leon <leonshamir1@gmail.com>
2024-09-13 21:21:12 -0500

    Fix: fix route import

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import HeroC from "./components/Hero/Hero";
 import NavBar from "./components/NavBar/NavBar";
 import Simplify from "./components/Simplify/Simplify";
 import Testimonials from "./components/Testimonials/Testimonials";
-import MainC from "./components/main/MainC";
+import MainC from "./components/Main/MainC";
 
 function App() {
   return (
